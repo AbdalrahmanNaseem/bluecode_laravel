247e2016316e27b0b4dcd785f5ce47509967fd78
AbdalrahmanNaseem <abdalrahmannaseem33@gmail.com>
2025-04-26 00:09:38 +0300

    - /naseem /-

--- database/migrations/2025_04_24_165406_create_topics_table.php
+++ database/migrations/2025_04_24_165406_create_topics_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('image')->nullable();
-            $table->string('supject')->nullable();
+            $table->text('supject')->change();
             $table->foreignIdFor(\Illuminate\Foundation\Auth\User::class);
             $table->foreignIdFor(\App\Models\lesson::class);
 
